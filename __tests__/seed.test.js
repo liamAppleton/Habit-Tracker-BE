315a701b7bad52e423415d7c976c18a641ebd7d1
Liam Appleton <liamappleton97@outlook.com>
2025-05-21 08:47:42 +0100

    passing test: habits table has name column of varying character

--- __tests__/seed.test.js
+++ __tests__/seed.test.js
@@ -115,5 +115,19 @@ describe('seed', () => {
           expect(column_name).toBe('habit_id');
         });
     });
+    test('habits table has name column of varying character', () => {
+      return db
+        .query(
+          `
+            SELECT column_name, data_type, column_default
+            FROM information_schema.columns
+            WHERE table_name = 'habits'
+            AND column_name = 'name'`
+        )
+        .then(({ rows: [column] }) => {
+          expect(column.column_name).toBe('name');
+          expect(column.data_type).toBe('character varying');
+        });
+    });
   });
 });
