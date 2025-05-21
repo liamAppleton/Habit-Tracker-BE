e8091a12acee7ca331d39366aea7c1175b4f8123
Liam Appleton <liamappleton97@outlook.com>
2025-05-21 08:51:16 +0100

    passing test: habits table has status column of varying character

--- __tests__/seed.test.js
+++ __tests__/seed.test.js
@@ -157,5 +157,19 @@ describe('seed', () => {
           expect(column.data_type).toBe('character varying');
         });
     });
+    test('habits table has status column of varying character', () => {
+      return db
+        .query(
+          `
+            SELECT column_name, data_type, column_default
+            FROM information_schema.columns
+            WHERE table_name = 'habits'
+            AND column_name = 'status'`
+        )
+        .then(({ rows: [column] }) => {
+          expect(column.column_name).toBe('status');
+          expect(column.data_type).toBe('character varying');
+        });
+    });
   });
 });
