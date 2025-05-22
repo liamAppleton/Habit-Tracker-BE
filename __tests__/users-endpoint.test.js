857ad6e8f81389f3bbb0229542b78f970e6933c4
Liam Appleton <liamappleton97@outlook.com>
2025-05-22 22:52:42 +0100

    passing test: 200: Responds with an array of user objects

--- __tests__/users-endpoint.test.js
+++ __tests__/users-endpoint.test.js
@@ -1,10 +1,31 @@
 const request = require('supertest');
 const app = require('../app.js');
-const db = require('../db/connection/js');
+const db = require('../db/connection.js');
 const seed = require('../db/seeds/seed.js');
 const data = require('../db/data/test-data');
 
 beforeEach(() => seed(data));
 afterAll(() => db.end());
 
-describe('GET /api/users', () => {});
+describe('GET /api/users', () => {
+  test('200: Responds with an array of user objects', () => {
+    return request(app)
+      .get('/api/users')
+      .expect(200)
+      .then(({ body: { users } }) => {
+        expect(users.length).not.toBe(0);
+
+        users.forEach((user) => {
+          expect(user).toEqual(
+            expect.objectContaining({
+              username: expect.any(String),
+              email: expect.any(String),
+              password: expect.any(String),
+              created_at: expect.any(String),
+              updated_at: expect.anything(),
+            })
+          );
+        });
+      });
+  });
+});
