a8c4a0bf9a6bc56fe102da38df76c7ecd11e3948
Liam Appleton <liamappleton97@outlook.com>
2025-05-23 23:28:46 +0100

    passing test: 404: Responds with "user not found" when passed a username that does not exist

--- __tests__/users-endpoint.test.js
+++ __tests__/users-endpoint.test.js
@@ -44,4 +44,13 @@ describe('GET /api/users/:username', () => {
         });
       });
   });
+  test('404: Responds with "user not found" when passed a username that does not exist', () => {
+    return request(app)
+      .get('/api/users/banana')
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.status).toBe(404);
+        expect(body.msg).toBe('user not found');
+      });
+  });
 });
