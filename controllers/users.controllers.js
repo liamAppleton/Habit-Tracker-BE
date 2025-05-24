752d0af0ab0ff150ebff5f1bcd5356fb0e476c23
Liam Appleton <liamappleton97@outlook.com>
2025-05-24 23:01:08 +0100

    creating postUser controller

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,16 +1,23 @@
-const { fetchUsers, fetchUserByUsername } = require('../models');
+const { fetchUsers, fetchUserByUsername, addUser } = require('../models');
 
-module.exports.getUsers = (req, res, next) => {
+exports.getUsers = (req, res, next) => {
   fetchUsers().then((users) => {
     res.status(200).send({ users });
   });
 };
 
-module.exports.getUserByUsername = (req, res, next) => {
+exports.getUserByUsername = (req, res, next) => {
   const { username } = req.params;
   fetchUserByUsername(username)
     .then((user) => {
       res.status(200).send({ user });
     })
     .catch((err) => next(err));
+};
+
+exports.postUser = (req, res, next) => {
+  const user = req.body;
+  addUser(user).then((user) => {
+    res.status(201).send({ user });
+  });
 };
