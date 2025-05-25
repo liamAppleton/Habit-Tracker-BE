e8c7fd521791617c4806023fbb0a0b2142271bfd
Liam Appleton <liamappleton97@outlook.com>
2025-05-25 23:31:07 +0100

    adding rows length check to updateUserByUsername model

--- models/users.models.js
+++ models/users.models.js
@@ -50,6 +50,9 @@ exports.updateUserByUsername = (username, body) => {
       [valueToUpdate, username]
     )
     .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: 'user not found' });
+      }
       return rows[0];
     });
 };
