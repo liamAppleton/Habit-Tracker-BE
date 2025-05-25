6f98a6feafb436b72d6d87191cfa90b9debc3311
Liam Appleton <liamappleton97@outlook.com>
2025-05-25 23:27:45 +0100

    adding catch block to patchUserByUsername controller

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -31,7 +31,9 @@ exports.postUser = (req, res, next) => {
 
 exports.patchUserByUsername = (req, res, next) => {
   const { username } = req.params;
-  updateUserByUsername(username, req.body).then((user) => {
-    res.status(200).send({ user });
-  });
+  updateUserByUsername(username, req.body)
+    .then((user) => {
+      res.status(200).send({ user });
+    })
+    .catch((err) => next(err));
 };
