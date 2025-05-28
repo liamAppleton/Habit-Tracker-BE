7390455c5b4e7b7282f7ae07c8fdf234b047b84a
Liam Appleton <liamappleton97@outlook.com>
2025-05-28 21:36:23 +0100

    removing status and updated_at from habits data

--- db/data/development-data/habits.js
+++ db/data/development-data/habits.js
@@ -1,22 +1,18 @@
 module.exports = [
   {
-    id: 1,
+    habit_id: 1,
     username: 'devuser1',
     name: 'Evening Run',
     frequency: 'Weekly',
-    status: 'Not Started',
     streak_count: 0,
     created_at: '2025-11-09T00:00:00.000Z',
-    updated_at: '2025-11-23T00:00:00.000Z',
   },
   {
-    id: 2,
+    habit_id: 2,
     username: 'devuser1',
     name: 'Drink Water',
     frequency: 'Daily',
-    status: 'In Progress',
     streak_count: 3,
     created_at: '2025-11-11T00:00:00.000Z',
-    updated_at: '2025-11-23T00:00:00.000Z',
   },
 ];
--- db/data/test-data/habits.js
+++ db/data/test-data/habits.js
@@ -1,22 +1,18 @@
 module.exports = [
   {
-    id: 1,
+    habit_id: 1,
     username: 'testuser1',
     name: 'Morning Yoga',
     frequency: 'Daily',
-    status: 'In Progress',
     streak_count: 5,
     created_at: '2025-11-20T00:00:00.000Z',
-    updated_at: '2025-11-23T00:00:00.000Z',
   },
   {
-    id: 2,
+    habit_id: 2,
     username: 'testuser1',
     name: 'Read Book',
     frequency: 'Weekly',
-    status: 'Not Started',
     streak_count: 0,
     created_at: '2025-11-23T00:00:00.000Z',
-    updated_at: '2025-11-23T00:00:00.000Z',
   },
 ];
