101de7cfacbf51ca5546b199f8248bdacb714066
Liam Appleton <liamappleton97@outlook.com>
2025-05-28 21:37:39 +0100

    creating fetchHabits model

--- models/index.js
+++ models/index.js
@@ -4,10 +4,12 @@ const {
   addUser,
   updateUserByUsername,
 } = require('./users.models');
+const { fetchHabits } = require('./habits.models');
 
 module.exports = {
   fetchUsers,
   fetchUserByUsername,
   addUser,
   updateUserByUsername,
+  fetchHabits,
 };
