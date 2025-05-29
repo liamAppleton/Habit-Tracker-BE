4fceecd47193581a3cbdfa3d5ed15f9229ebf6d1
Liam Appleton <liamappleton97@outlook.com>
2025-05-29 22:08:39 +0100

    passing test: 404: Responds with "habit not found" when passed a habit_id that does not exist

--- __tests__/habits-endpoint.test.js
+++ __tests__/habits-endpoint.test.js
@@ -55,4 +55,13 @@ describe('GET /api/habits/:habit_id', () => {
         expect(body.msg).toBe('bad request');
       });
   });
+  test('404: Responds with "habit not found" when passed a habit_id that does not exist', () => {
+    return request(app)
+      .get('/api/habits/99999')
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.status).toBe(404);
+        expect(body.msg).toBe('habit not found');
+      });
+  });
 });
