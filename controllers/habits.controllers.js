680b79cbcc51b784d6e80d8e4c59a455e5fd7268
Liam Appleton <liamappleton97@outlook.com>
2025-05-29 21:58:26 +0100

    creating getHabitById controller

--- controllers/habits.controllers.js
+++ controllers/habits.controllers.js
@@ -1,7 +1,14 @@
-const { fetchHabits } = require('../models');
+const { fetchHabits, fetchHabitById } = require('../models');
 
 exports.getHabits = (req, res, next) => {
   fetchHabits().then((habits) => {
     res.status(200).send({ habits });
+  });
+};
+
+exports.getHabitById = (req, res, next) => {
+  const { habit_id } = req.params;
+  fetchHabitById(habit_id).then((habit) => {
+    res.status(200).send({ habit });
   });
 };
