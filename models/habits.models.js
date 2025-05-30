e823f0a96f86fa8e83b2108015c89d12952bdf6e
Liam Appleton <liamappleton97@outlook.com>
2025-05-30 19:43:24 +0100

    creating addHabit model

--- models/habits.models.js
+++ models/habits.models.js
@@ -1,5 +1,6 @@
 const db = require('../db/connection.js');
 const { throwError } = require('../utils/utils.js');
+const format = require('pg-format');
 
 exports.fetchHabits = () => {
   return db.query('SELECT * FROM habits').then(({ rows }) => {
@@ -14,4 +15,17 @@ exports.fetchHabitById = (habit_id) => {
       if (rows.length === 0) return throwError(404, 'habit not found');
       return rows[0];
     });
+};
+
+exports.addHabit = ({ username, name, frequency }) => {
+  const queryString = format(
+    `
+    INSERT INTO habits
+  (username, name, frequency, streak_count, created_at)
+  VALUES %L RETURNING *`,
+    [[username, name, frequency, 0, new Date()]]
+  );
+  return db.query(queryString).then(({ rows }) => {
+    return rows[0];
+  });
 };
