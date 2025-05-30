d09d7224570b8919f77c3551bfe7f416750f43a8
Liam Appleton <liamappleton97@outlook.com>
2025-05-30 20:02:00 +0100

    checkExists passing test: should return true when value does exist

---
+++ __tests__/utils.test.js
@@ -0,0 +1,12 @@
+const { checkExists } = require('../utils/utils');
+const db = require('../db/connection');
+
+afterAll(() => db.end());
+
+describe('checkExists()', () => {
+  test('should return true when value does exist', () => {
+    return checkExists('users', 'username', 'testuser1').then((result) => {
+      expect(result).toBe(true);
+    });
+  });
+});
