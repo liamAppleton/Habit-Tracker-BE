8044301b1d1f89056222d8f7e2dad545341ece4d
Liam Appleton <liamappleton97@outlook.com>
2025-06-09 10:59:42 +0100

    removing status column

--- db/data/development-data/habitLogs.js
+++ db/data/development-data/habitLogs.js
@@ -1,20 +1,17 @@
 module.exports = [
   {
-    log_id: 'log1',
+    log_id: 1,
     habit_id: 1,
     date: '2025-05-18T00:00:00.000Z',
-    status: 'Completed',
   },
   {
-    log_id: 'log2',
+    log_id: 2,
     habit_id: 2,
     date: '2025-05-18T00:00:00.000Z',
-    status: 'Completed',
   },
   {
-    log_id: 'log3',
+    log_id: 3,
     habit_id: 2,
     date: '2025-05-19T00:00:00.000Z',
-    status: 'Completed',
   },
 ];
